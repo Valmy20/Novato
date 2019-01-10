660d6f5b3eba070a2248f0267a33431f3efa2731
Valmy <valmyericles@gmail.com>
2019-01-10 14:15:59 -0300

    change ckeditor model to original mode

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,9 +1,7 @@
-module Ckeditor
-  class Asset < ActiveRecord
-    include Ckeditor::Orm::ActiveRecord::AssetBase
+class Ckeditor::Asset < ActiveRecord::Base
+  include Ckeditor::Orm::ActiveRecord::AssetBase
 
-    delegate :url, :current_path, :content_type, to: :data
+  delegate :url, :current_path, :content_type, to: :data
 
-    validates :data, presence: true
-  end
+  validates :data, presence: true
 end
--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,9 +1,7 @@
-module Ckeditor
-  class AttachmentFile < Ckeditor::Asset
-    mount_uploader :data, CkeditorAttachmentFileUploader, mount_on: :data_file_name
+class Ckeditor::AttachmentFile < Ckeditor::Asset
+  mount_uploader :data, CkeditorAttachmentFileUploader, mount_on: :data_file_name
 
-    def url_thumb
-      @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-    end
+  def url_thumb
+    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
   end
 end
