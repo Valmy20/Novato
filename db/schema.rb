c0efbcbb2ab228ed084ff9db5713a9d1a19c93d5
Valmy <valmyericles@gmail.com>
2019-01-25 16:19:32 -0300

    change migration phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_010213) do
+ActiveRecord::Schema.define(version: 2019_01_25_191239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -198,7 +198,6 @@ ActiveRecord::Schema.define(version: 2019_01_20_010213) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "phone"
     t.string "password_digest"
     t.string "avatar"
     t.string "cover"
@@ -211,6 +210,7 @@ ActiveRecord::Schema.define(version: 2019_01_20_010213) do
     t.boolean "deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
   add_foreign_key "ass_post_categories", "categories"
